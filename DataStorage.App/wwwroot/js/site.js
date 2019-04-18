9622a307c2c752c06056601cb344cb36532fe799
Denis <denis.miroshnichenko.ukr@mail.ru>
2019-04-18 19:23:08 +0300

    Simple UI for UserStorage + AJAX return documentId

--- DataStorage.App/wwwroot/js/site.js
+++ DataStorage.App/wwwroot/js/site.js
@@ -15,4 +15,21 @@
         //alert();
         $(this).parent().focus();
     } 
+
+    $('.fileblock').on("click", function(e) {
+        e.preventDefault();
+        var str = $(this).find('#hiddenblockid').val();
+        $.ajax({
+            type:"POST",
+            data: JSON.stringify(str),
+            url: '/Document/UserStorage'
+        }).done(function(res){
+            alert(str);
+        });
+    })
+
+    $('.fileblock').on("contextmenu", function(e) {
+        e.preventDefault();
+        alert("Right click (context menu)");
+    })
 })
